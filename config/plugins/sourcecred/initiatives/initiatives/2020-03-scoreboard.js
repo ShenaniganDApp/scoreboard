f9d12fcfe3f78fdebdec59d7ff097a14b72f452e
youngkidwarrior <youngkidwarrior@users.noreply.github.com>
2021-03-29 01:44:22 +0000

    Deploying to gh-pages from  @ 8806192533b24bb52af4742814966f1de85d3bbd 🚀

---
+++ config/plugins/sourcecred/initiatives/initiatives/2020-03-scoreboard.js
@@ -0,0 +1,28 @@
+[
+	{
+		type: 'sourcecred/initiativeFile',
+		version: '0.2.0',
+	},
+	{
+		title: 'Scoreboard',
+		timestampIso: '2021-02-07T10:00:00.000Z',
+		weight: {
+			incomplete: 0,
+			complete: 0,
+		},
+		completed: true,
+		champions: [],
+		dependencies: {},
+		references: {},
+		contributions: {
+			entries: [
+				{
+					title: 'Scoreboard dev work and distributions',
+					timestampIso: '2021-01-15:00:00.000Z',
+					weight: 8,
+					contributors: ['https://github.com/youngkidwarrior'],
+				},
+			],
+		},
+	},
+];
