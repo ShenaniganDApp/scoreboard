d8be82bdb75cf25647e7e09e62a9e6825365ada6
VictorGinelli <ginelliunit@gmail.com>
2021-08-30 22:46:04 -0700

    lp Rewards finished

--- rewards/lpScript.js
+++ rewards/lpScript.js
@@ -1,3 +1,4 @@
+const sc = require('sourcecred').sourcecred;
 const _ = require('lodash');
 
 const fs = require('fs');
@@ -6,30 +7,72 @@ const BigNumber = require('bignumber.js');
 const fetch = require('node-fetch');
 const queries = require('./queries');
 const snapshot = require('erc20-snapshot');
+const { forEach } = require('lodash');
 const readFile = util.promisify(fs.readFile);
 const EPOCHS_PATH = './lastRewardEpoch.json';
+const Ledger = sc.ledger.ledger.Ledger;
+
+const ethers = require('ethers');
+
 const ADDRESS_BOOK_PATH = '../data/addressbook.json';
+const LEDGER_PATH = '../data/ledger.json';
+
+const NodeAddress = sc.core.address.makeAddressModule({
+  name: 'NodeAddress',
+  nonce: 'N',
+  otherNonces: new Map().set('E', 'EdgeAddress'),
+});
+
 const rewardsPairs = [
   '0xa527dbc7cdb07dd5fdc2d837c7a2054e6d66daf4',
   '0xaaefc56e97624b57ce98374eb4a45b6fd5ffb982',
 ];
 const oneWeekInBlocks = 120992;
 const oneDayInBlocks = 17284;
+
+const provider = new ethers.providers.JsonRpcProvider(
+  'https://rpc.xdaichain.com/'
+);
+const getTimeData = async (blockNumber) => {
+  const startUnixTimestamp =
+    (await (await provider.getBlock(blockNumber)).timestamp) * 1000;
+  const endUnixTimestamp =
+    (await (await provider.getBlock(blockNumber + oneWeekInBlocks)).timestamp) *
+    1000;
+  return { startUnixTimestamp, endUnixTimestamp };
+};
+
 (async () => {
   const data = await JSON.parse((await readFile(EPOCHS_PATH)).toString());
+  const ledgerJSON = (await readFile(LEDGER_PATH)).toString();
   const addressbook = await JSON.parse(
     (await readFile(ADDRESS_BOOK_PATH)).toString()
   );
-  let dailyWeightedRewards = [];
+  const addressBookMap = _.keyBy(addressbook, 'address');
+  const AddressMap = _.keyBy(addressbook, 'discordId');
+
+  const accountsJSON = JSON.parse(
+    (await readFile('../output/accounts.json')).toString()
+  );
+
+  const UserMap = _.keyBy(accountsJSON.accounts, 'account.identity.id');
+
+  const ledger = Ledger.parse(ledgerJSON);
+  let accounts = ledger.accounts();
+
+  let weightedRewardsPerEpoch = [];
+  let timeDataByAddresses = {};
+  let timeWeightPeriods = [];
   let weekCount = 0;
   for (let k = data.startBlock; k < data.endBlock; k += oneWeekInBlocks) {
     let dayCount = 0;
     let reserveUSDJSON = [];
+
+    // reserve for each token
     let reserveUSDSums = [];
     let liquidityTotals = [];
     let liquidityWeights = [];
     let blockWeekCounter = data.startBlock + oneWeekInBlocks * weekCount;
-    console.log('blockWeekCounter: ', blockWeekCounter);
     for (
       let i = blockWeekCounter;
       i < oneWeekInBlocks + blockWeekCounter;
@@ -81,211 +124,277 @@ const oneDayInBlocks = 17284;
           ...liquidityTotals[dayCount],
         };
       });
-      if (!dailyWeightedRewards[weekCount])
-        dailyWeightedRewards[weekCount] = [];
-      weightedDayRewards = {};
+      weightedReward = {};
+
+      // Filter for wallets in the addressbook
+      // multiply each token balance by the reserveUSD of the pool
+      // then divide it by the total amount of tokens
       rewardsPairs.forEach((address) => {
-        weightedDayRewards[address] = balances[address].map(
-          ({ wallet, balance }) => {
-            //filter addressbook accounts. THIS IS SLOW
-            addressbook.forEach(({ address }) => {
-              if (address === wallet)
-                return {
-                  wallet,
-                  balance: new BigNumber(balance)
-                    .multipliedBy(
-                      new BigNumber(reserveUSDJSON[dayCount][address])
-                    )
-                    .dividedBy(
-                      new BigNumber(liquidityTotals[dayCount][address])
-                    ),
-                };
-            });
-          }
-        );
+        weightedReward[address] = balances[address]
+
+          .filter(({ wallet }) => {
+            return addressbook.find(
+              ({ address: userAddress }) => userAddress === wallet
+            );
+          })
+          .map(({ wallet, balance }) => {
+            return {
+              wallet,
+              blockNumber: i,
+              balance: new BigNumber(balance)
+                .multipliedBy(new BigNumber(reserveUSDJSON[dayCount][address]))
+                .dividedBy(new BigNumber(liquidityTotals[dayCount][address])),
+            };
+          });
       });
-      dailyWeightedRewards[weekCount].push(weightedDayRewards);
+      weightedRewardsPerEpoch.push({ epochBlock: i, ...weightedReward });
       dayCount++;
     }
 
     weekCount++;
   }
+
+  // calculate the time periods for time weighting per wallet
+  for (let i = 0; i < weightedRewardsPerEpoch.length; i++) {
+    rewardsPairs.forEach((address) => {
+      const dayRewards = weightedRewardsPerEpoch[i][address];
+      for (let j = 0; j < dayRewards.length; j++) {
+        const { wallet, blockNumber, balance } = dayRewards[j];
+        if (
+          wallet in timeDataByAddresses &&
+          address in timeDataByAddresses[wallet]
+        ) {
+          const { startBlockNumber, endBlockNumber, previousBalance, weight } =
+            timeDataByAddresses[wallet][address];
+          if (!timeWeightPeriods[wallet]) timeWeightPeriods[wallet] = [];
+
+          // when balance is 0 rewards timer starts over for future rewards
+          if (i === weightedRewardsPerEpoch.length - 1) {
+            timeWeightPeriods = [
+              {
+                wallet,
+                startBlockNumber,
+                endBlockNumber,
+                weight,
+                tokenAddress: address,
+              },
+              ...timeWeightPeriods,
+            ];
+          } else if (balance.eq(0) && !previousBalance.eq(0)) {
+            timeWeightPeriods = [
+              {
+                wallet,
+                startBlockNumber,
+                endBlockNumber,
+                weight,
+                tokenAddress: address,
+              },
+              ...timeWeightPeriods,
+            ];
+
+            timeDataByAddresses[wallet][address] = {
+              startBlockNumber: blockNumber,
+              endBlockNumber: blockNumber,
+              previousBalance,
+              weight: 1,
+              ...timeDataByAddresses[wallet][address],
+            };
+          } else if (balance < previousBalance) {
+            timeWeightPeriods = [
+              {
+                wallet,
+                startBlockNumber,
+                endBlockNumber,
+                weight,
+                tokenAddress: address,
+              },
+              ...timeWeightPeriods,
+            ];
+            // when balance is less than previous epoch, rewards are calculated at current weight, then continue
+            timeDataByAddresses[wallet][address] = {
+              startBlockNumber: blockNumber,
+              endBlockNumber: blockNumber,
+              previousBalance,
+              ...timeDataByAddresses[wallet][address],
+            };
+          } else {
+            // If balance is greater than or equal to the previous epoch, continue
+            const daysStaked = (endBlockNumber - startBlockNumber) / 17284;
+            const monthsStaked = daysStaked / 30;
+            let weight = 0;
+            if (monthsStaked >= 1 && monthsStaked < 2) {
+              weight = 2;
+            } else if (monthsStaked >= 2) {
+              weight = 3;
+            } else {
+              weight = 1;
+            }
+            timeDataByAddresses[wallet][address] = {
+              startBlockNumber,
+              endBlockNumber: blockNumber,
+              previousBalance: balance,
+              weight,
+            };
+          }
+        } else {
+          if (wallet in timeDataByAddresses) {
+            timeDataByAddresses[wallet][address] = {
+              startBlockNumber: blockNumber,
+              endBlockNumber: blockNumber,
+              previousBalance: new BigNumber(0),
+              weight: 1,
+            };
+          } else {
+            timeDataByAddresses[wallet] = {};
+            timeDataByAddresses[wallet][address] = {
+              startBlockNumber: blockNumber,
+              endBlockNumber: blockNumber,
+              previousBalance: new BigNumber(0),
+              weight: 1,
+            };
+          }
+        }
+      }
+    });
+  }
+
+  // Multiply rewards by weight
+  timeWeightPeriods.forEach((period) => {
+    const { wallet, startBlockNumber, endBlockNumber, weight, tokenAddress } =
+      period;
+    weightedRewardsPerEpoch.forEach((epoch) => {
+      const { epochBlock } = epoch;
+      if (
+        weight > 1 &&
+        epochBlock >= startBlockNumber &&
+        epochBlock < endBlockNumber
+      ) {
+        // This is a O(n)^2 problem since we search all addresses for every epoch
+        // it would be better to restructure so we can do a single search for
+        // every address in a given time period and update its value
+        epoch[tokenAddress]
+          .find((position) => position.wallet === wallet)
+          .balance.multipliedBy(weight);
+      }
+    });
+  });
+
+  // Calculate weekly maximums and normalize
+
+  for (let k = data.startBlock; k < data.endBlock; k += oneWeekInBlocks) {
+    let weekTotalRewards = {};
+    let weekSum = new BigNumber(0);
+    weightedRewardsPerEpoch.forEach((epoch) => {
+      const epochBlock = epoch.epochBlock;
+      if (epochBlock >= k && epochBlock < k + oneWeekInBlocks) {
+        rewardsPairs.forEach((address) => {
+          epoch[address].forEach((position) => {
+            const { wallet, balance } = position;
+            if (weekTotalRewards[wallet] !== undefined) {
+              const previousMax = weekTotalRewards[wallet];
+              weekTotalRewards[wallet] = previousMax.plus(balance);
+              weekSum = weekSum.plus(balance);
+            } else {
+              weekTotalRewards[wallet] = balance;
+              weekSum = weekSum.plus(balance);
+            }
+          });
+        });
+      }
+    });
+    Object.keys(weekTotalRewards).forEach((address) => {
+      weekTotalRewards[address] = weekTotalRewards[address]
+        .dividedBy(weekSum)
+        .multipliedBy(13)
+        .toString();
+    });
+
+    //Write this weeks scores into an initiative
+
+    const accountCred = Object.keys(weekTotalRewards).map((wallet) => {
+      const discordId = addressBookMap[wallet].discordId;
+      const cred = weekTotalRewards[wallet];
+      return { discordId, cred };
+    });
+
+    const accountCredMap = _.keyBy(accountCred, 'discordId');
+    const discordAcc = accounts
+      .map((a) => {
+        const credAcc = UserMap[a.identity.id];
+        if (!credAcc) return null;
+        if (a.identity.subtype !== 'USER') return null;
+        const discordAliases = a.identity.aliases.filter((alias) => {
+          const parts = NodeAddress.toParts(alias.address);
+          return parts.indexOf('discord') > 0;
+        });
+        let amount = null;
+        let discordId = null;
+        discordAliases.forEach((alias) => {
+          discordId = NodeAddress.toParts(alias.address)[4];
+          if (AddressMap[discordId]) {
+            if (accountCredMap[discordId]) {
+              amount = accountCredMap[discordId].cred;
+            }
+          }
+        });
+
+        return amount
+          ? {
+              ...a,
+              amount,
+            }
+          : null;
+      })
+      .filter(Boolean);
+
+    // Get block timestamp
+    const { startUnixTimestamp, endUnixTimestamp } = await getTimeData(k);
+
+    const startDateISO = new Date(startUnixTimestamp).toISOString();
+
+    const endDateISO = new Date(endUnixTimestamp).toISOString();
+
+    const startDateLocale = new Date(startUnixTimestamp).toLocaleString();
+    const endDateLocale = new Date(endUnixTimestamp).toLocaleString();
+
+    const endDateYear = startDateISO.split('T')[0].split('-')[0];
+    const endDateMonth = startDateISO.split('T')[0].split('-')[1];
+
+    const entries = discordAcc.map((a) => {
+      return `
+        {
+          "title": "Liquidity Provider: @${a.identity.name}",
+          "timestampIso": "${endDateISO}",
+          "weight": ${a.amount},
+          "contributors": ["@${a.identity.name}"]
+        }`;
+    });
+
+    const fileJson = `[
+      {
+        "type": "sourcecred/initiativeFile",
+        "version": "0.2.0"
+      },
+      {
+        "title": "Liquidity Rewards Week of ${startDateLocale} to ${endDateLocale} ",
+        "timestampIso": "${endDateISO}",
+        "weight": {
+          "incomplete": 0,
+          "complete": 0
+        },
+        "completed": true,
+        "champions": [],
+        "dependencies": {},
+        "references": {},
+        "contributions":{ 
+          "entries": [${entries}]
+      }
+    }]`;
+    fs.writeFileSync(
+      `../config/plugins/sourcecred/initiatives/initiatives/${endDateYear}-${endDateMonth}-liquidity-rewards-${
+        endDateISO.split('T')[0]
+      }`,
+      fileJson
+    );
+  }
 })();
-
-// const fetchMintsandBurns = async (address) => {
-//   console.log('Fetching Mints and Burns...');
-//   const {
-//     data: { mints },
-//   } = await (
-//     await fetch('https://api.thegraph.com/subgraphs/name/1hive/uniswap-v2', {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json',
-//         Accept: 'application/json',
-//       },
-//       body: JSON.stringify({ query: queries.mints(address) }),
-//     })
-//   ).json();
-//   const {
-//     data: { burns },
-//   } = await (
-//     await fetch('https://api.thegraph.com/subgraphs/name/1hive/uniswap-v2', {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json',
-//         Accept: 'application/json',
-//       },
-//       body: JSON.stringify({ query: queries.burns(address) }),
-//     })
-//   ).json();
-//   const mintHashes = [];
-//   const burnHashes = [];
-//   for (let i = 0; i < mints.length; i++) {
-//     mintHashes.push(mints[i].transaction.id);
-//   }
-//   for (let i = 0; i < burns.length; i++) {
-//     burnHashes.push(burns[i].transaction.id);
-//   }
-//   const chunkedMintHashes = _.chunk(mintHashes, 50);
-//   const chunkedBurnHashes = _.chunk(burnHashes, 50);
-
-//   const finalMints = [];
-//   const finalBurns = [];
-//   for (let i = 0; i < chunkedMintHashes.length; i++) {
-//     const { data } = await (
-//       await fetch(`https://blockscout.com/poa/xdai/graphiql`, {
-//         method: 'POST',
-//         headers: {
-//           'Content-Type': 'application/json',
-//           Accept: 'application/json',
-//         },
-//         body: JSON.stringify({ query: queries.txSender(chunkedMintHashes[i]) }),
-//       })
-//     ).json();
-//     const txDataArray = Object.keys(data).reduce((acc, curr) => {
-//       return [...acc, { ...data[curr] }];
-//     }, []);
-//     txDataArray.forEach((tx) => {
-//       mints.forEach((mint) => {
-//         if (mint.transaction.id === tx.hash) {
-//           finalMints.push({ fromAddressHash: tx.fromAddressHash, ...mint });
-//         }
-//       });
-//     });
-//   }
-//   for (let i = 0; i < chunkedBurnHashes.length; i++) {
-//     const { data } = await (
-//       await fetch(`https://blockscout.com/poa/xdai/graphiql`, {
-//         method: 'POST',
-//         headers: {
-//           'Content-Type': 'application/json',
-//           Accept: 'application/json',
-//         },
-//         body: JSON.stringify({ query: queries.txSender(chunkedBurnHashes[i]) }),
-//       })
-//     ).json();
-//     const txDataArray = Object.keys(data).reduce((acc, curr) => {
-//       return [...acc, { ...data[curr] }];
-//     }, []);
-//     txDataArray.forEach((tx) => {
-//       burns.forEach((burn) => {
-//         if (burn.transaction.id === tx.hash) {
-//           finalBurns.push({ fromAddressHash: tx.fromAddressHash, ...burn });
-//         }
-//       });
-//     });
-//   }
-//   return { mints: finalMints, burns: finalBurns };
-// };
-
-// const getAccountTotalsBeforeTimestamp = (totalMints, totalBurns, timestamp) => {
-//   const totalAccountMint = Object.keys(totalMints).forEach((account) => {
-//     totalMints[account]
-//       .filter((mint) => {
-//         return mint.timestamp < timestamp;
-//       })
-//       .reduce(
-//         (acc, mint) => {
-//           console.log('acc: ', acc);
-//           if (mint.liquidity)
-//             return acc.liquidity.plus(new BigNumber(mint.liquidity));
-//         },
-//         { liquidity: new BigNumber(0) }
-//       );
-//   });
-//   const totalAccountBurn = Object.keys(totalBurns).forEach((account) => {
-//     totalBurns[account]
-//       .filter((burn) => {
-//         return burn.timestamp < timestamp;
-//       })
-//       .reduce(
-//         (acc, burn) => {
-//           if (burn.liquidity)
-//             return burn.acc.liquidity.plus(new BigNumber(burn.liquidity));
-//         },
-//         { liquidity: new BigNumber(0) }
-//       );
-//   });
-
-//   return { totalAccountMint, totalAccountBurn };
-// };
-
-// // const getTotalLiquidityAtTimestamp = (timestamp) => {
-// //   if (!xdaiMintsAndBurns || !hnyMintsAndBurns) return;
-// //   const totalMints = _.reduce(
-// //     xdaiMintsAndBurns.mints,
-// //     (acc, mint) => {
-// //       if (mint.timestamp < timestamp) {
-// //         acc[mint.fromAddressHash] = acc[mint.fromAddressHash] || 0;
-// //         acc[mint.fromAddressHash] += mint.value;
-// //       }
-// //       return acc;
-// //     },
-// //     {}
-// //   );
-// //   const totalBurns = _.reduce(
-// //     burns,
-// //     (acc, burn) => {
-// //       if (burn.timestamp < timestamp) {
-// //         acc[burn.fromAddressHash] = acc[burn.fromAddressHash] || 0;
-// //         acc[burn.fromAddressHash] -= burn.value;
-// //       }
-// //       return acc;
-// //     },
-// //     {}
-// //   );
-// //   return { mints, burns, totalMints, totalBurns };
-// // };
-
-// (async () => {
-//   const startTimestamp = 1614585600;
-//   const endTimestamp = startTimestamp + 604800;
-//   hnyMintsAndBurns = await fetchMintsandBurns(
-//     '0xaaefc56e97624b57ce98374eb4a45b6fd5ffb982'
-//   );
-//   xdaiMintsAndBurns = await fetchMintsandBurns(
-//     '0xa527dbc7cdb07dd5fdc2d837c7a2054e6d66daf4'
-//   );
-//   const accountMintsAndBurnsHNY = {
-//     mints: _.groupBy(hnyMintsAndBurns.mints, 'fromAddressHash'),
-//     burns: _.groupBy(hnyMintsAndBurns.burns, 'fromAddressHash'),
-//   };
-//   const accountMintsAndBurnsXDAI = {
-//     mints: _.groupBy(xdaiMintsAndBurns.mints, 'fromAddressHash'),
-//     burns: _.groupBy(xdaiMintsAndBurns.burns, 'fromAddressHash'),
-//   };
-
-//   const hnyLiquidityTotals = getAccountTotalsBeforeTimestamp(
-//     accountMintsAndBurnsHNY.mints,
-//     accountMintsAndBurnsHNY.burns,
-//     startTimestamp
-//   );
-//   console.log('hnyLiquidityTotals: ', hnyLiquidityTotals);
-//   const xdaiLiquidityTotals = getAccountTotalsBeforeTimestamp(
-//     accountMintsAndBurnsXDAI.mints,
-//     accountMintsAndBurnXDAI.burns,
-//     startTimestamp
-//   );
-
-//   console.log('accountsMintsBeforeTimestamp: ', accountsMintsBeforeTimestamp);
-// })();
