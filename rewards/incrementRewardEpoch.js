47559f7c7badb610d13fb8556611ccfcc479bee8
youngkidwarrior <ginelliunit@gmail.com>
2021-10-17 19:39:27 -0700

    Automate SC

---
+++ rewards/incrementRewardEpoch.js
@@ -0,0 +1,16 @@
+const { error } = require('console');
+const fs = require('fs');
+const util = require('util');
+const readFile = util.promisify(fs.readFile);
+const EPOCHS_PATH = 'rewards/lastRewardEpoch.json';
+
+const oneWeekInBlocks = 120992;
+
+(async () => {
+  const epochData = await JSON.parse((await readFile(EPOCHS_PATH)).toString());
+  epochData.endBlock = epochData.endBlock + oneWeekInBlocks;
+  const data = await JSON.stringify(epochData);
+  await fs.writeFile('rewards/lastRewardEpoch.json', data, (err) => {
+    if (err) console.log(Error(err));
+  });
+})();
